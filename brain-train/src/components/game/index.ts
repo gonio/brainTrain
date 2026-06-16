eb577890c353a0e10cde0dda77f0276a4c2f46fb
DOKE <xxzoing@qq.com>
2026-06-17 03:08:07 +0800

    ✨ 新增 QuestLevelIntro 关卡规则说明卡组件

--- brain-train/src/components/game/index.ts
+++ brain-train/src/components/game/index.ts
@@ -4,4 +4,6 @@ export { Timer, CountdownTimer } from './Timer';
 export { SchulteGrid } from './SchulteGrid';
 export { StroopGame } from './StroopGame';
 export { SequenceGame } from './SequenceGame';
+export { BottleGame } from './BottleGame';
 export { GameInstructionsDialog } from './GameInstructions';
+export { QuestLevelIntro } from './QuestLevelIntro';
