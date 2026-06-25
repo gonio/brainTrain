315badad982957cc23e0f061da3c35b28207d373
DOKE <xxzoing@qq.com>
2026-06-25 21:26:56 +0800

    feat(quest): QuestHub/QuestHUD/QuestResultDialog UI 组件
    - QuestHUD: 进行中顶部条（游戏名+难度+退出）
    - QuestHub: 4 游戏进度条+星级展示+开始下一关
    - QuestResultDialog: 星级动画+继续下一关/返回大厅
    - index.ts barrel export

---
+++ brain-train/src/components/quest/index.ts
@@ -0,0 +1,5 @@
+export { QuestRunner } from './QuestRunner';
+export type { RunnerProps } from './QuestRunner';
+export { QuestHub } from './QuestHub';
+export { QuestHUD } from './QuestHUD';
+export { QuestResultDialog } from './QuestResultDialog';
